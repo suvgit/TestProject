c57e0e7162990a2ae74792fc2e73606d31b7cd0e
suvgit <sarithavusuresh@gmail.com>
2017-03-16 12:47:41 +0000

    first commit...first version

---
+++ TravelSystem/src/model/mypackage/Reservation.java
@@ -0,0 +1,42 @@
+package model.mypackage;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Reservation
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public String name;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Reservation(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public void reserve() {
+		// TODO implement me
+	}
+
+}
+
---
+++ TravelSystem/src/model/mypackage/User.java
@@ -0,0 +1,31 @@
+package model.mypackage;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class User
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public int userid;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public User(){
+		super();
+	}
+
+}
+
